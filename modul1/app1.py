f1c3c26ef39a69115ba1586e927c5542a0edc5b2
emcraciu <ozy24us@gmail.com>
2024-10-09 21:16:40 +0300

    part1

--- modul1/app1.py
+++ modul1/app1.py
@@ -1,2 +1,26 @@
 # create triangle or square using print
 
+
+print("      *")
+print("     ***")
+print("    *****")
+print("   *******")
+print("  *********")
+
+print(" _______")
+print("|       |")
+print("|       |")
+print("|       |")
+print(" _______")
+
+print("      *")
+print("     * *")
+print("    *   *")
+print("   *     *")
+print("  * * * * *")
+
+print("      ^")
+print("     / \\")
+print("    /   \\")
+print("   /     \\")
+print("  / _ _ _ \\")
--- modul1/part1.py
+++ modul1/part1.py
@@ -5,4 +5,4 @@ print("Hello World", end="#")
 print("Hello World", "New Text", "Third Text", sep="!", end="-")
 print("should be on new line")
 
-
+print("\"")
