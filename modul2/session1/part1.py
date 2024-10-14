fe6e4c7f9e52988b8957722b7e1d9fddc83ceab4
emcraciu <ozy24us@gmail.com>
2024-10-14 19:11:30 +0300

    part1

---
+++ modul2/session1/part1.py
@@ -0,0 +1,50 @@
+# variables
+
+my_str = "Hello World"
+
+print(
+    my_str,
+    "New String"
+)
+
+my_str = "Hello World2"
+
+print(
+    my_str,
+    "New String"
+)
+
+my_str = 100000
+print(
+    my_str,
+    "New String"
+)
+
+# compare
+print(my_str == "100000")  ## str object not equal to int bject
+
+# operations
+result = 100 + 100
+print(result)
+
+result = result + 10
+print(result)
+
+result += 10
+print(result)
+
+result = 100 * 100
+print(result)
+
+result *= 3
+print(result)
+print(type(result))
+
+result = 10/2
+print(result)
+print(type(result))
+
+result = 5.5
+print(result)
+print(type(result))
+
