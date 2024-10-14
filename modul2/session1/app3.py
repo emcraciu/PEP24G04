d59128f35d8e973ea5c0e063fb91787e764a46df
emcraciu <ozy24us@gmail.com>
2024-10-14 20:19:36 +0300

    part1

---
+++ modul2/session1/app3.py
@@ -0,0 +1,11 @@
+a = input("Valoarea primului numar: ")
+b = input("Valoarea celui de al 2 lea numar: ")
+
+# a
+c = (int(a) + int(b)) / 2
+print(c)
+
+# b
+impartire = int(a) / int(b)
+print(impartire)
+print(int(impartire))
