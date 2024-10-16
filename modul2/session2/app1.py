9b36c1ab67d411a2afc1f7398c88c7307713428f
emcraciu <ozy24us@gmail.com>
2024-10-16 19:21:15 +0300

    part2

---
+++ modul2/session2/app1.py
@@ -0,0 +1,10 @@
+string1 = 'Astazi ma duc la "facultate".'
+print(string1)
+
+string2 = r"""/*\/*\*/*\/*\
+Python
+\./\./\./\./"""
+print(string2)
+
+string3 = 'P\ty\tt\th\to\tn'
+print(string3)
---
+++ modul2/session2/app2.py
@@ -0,0 +1,5 @@
+nume = input("Cum te numesti?  ")
+varsta = input("Ce varsta ai? ")
+an_curent = 2024
+an = an_curent - int(varsta)
+print(f"Ceau {nume}! Deci te-ai nascut in {an}")
