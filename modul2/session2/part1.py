44958ac78d19b8251d894ec704c09a028730014d
emcraciu <ozy24us@gmail.com>
2024-10-16 18:59:42 +0300

    part2

---
+++ modul2/session2/part1.py
@@ -0,0 +1,30 @@
+# strings
+
+string1 = u"Hello World {}"
+string2 = r'\n\tHello World\n'
+print(string2)
+string3 = """
+    Hello World
+"""
+string4 = '''Hello World'''
+
+# format method
+string1 = "Hello World {}"
+result = string1.format("Test")
+print(result)
+
+string1 = "Hello World {1}, {1}, {0}"
+result = string1.format("Test0", 'Test1', 'Test2')
+print(result)
+
+string1 = "Hello World {test1}, {test2}, {test3}"
+result = string1.format(test1="Test0", test2='Test1', test3='Test2')
+print(result)
+
+# formated string
+var1 = input("Add to message: ")
+var2 = "End"
+string1 = f"Hello World {var1} {var1} {var2}"
+print(string1)
+
+
