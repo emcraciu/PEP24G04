b7345efb67cd5cbfb1c37bdb3c8204c8e69070b0
Bogdan-1984 <dragos.bogdan1984@gmail.com>
2024-10-17 16:01:26 +0300

    Ultimul exercitiu adaugat

--- modul2/session1/Exercitii S1.py
+++ modul2/session1/Exercitii S1.py
@@ -50,4 +50,22 @@ nr4 = int(input("Introduceti valoarea numarului 4: "))
 punct_c = nr3 ** nr4
 print(f"{nr1} la puterea {nr2} este: {punct_c}")
 
-#updated
+# ----------------------------------------------------------------------------------------------------------------
+
+# Se cere input de la utilizator cu venit net pe luna. Din acesta, calculati regula 50/30/20:
+# - 50% din venit acordat necesităților (cumparaturi, intretinere, chirie, etc)
+# - 30% din venit acordat scopurilor recreative
+# - 20% din venit acordat datoriilor și economiilor
+
+venit_net = float(input("Venitul lunar este de: "))
+# venit_net = int(input("Venitul lunar este de: "))
+
+ron = "RON"
+necesitati = venit_net * 0.5
+recreative = venit_net * 0.3
+economii =  venit_net * 0.2
+
+print(f"Din venit lunar de {venit_net} {ron} aveti nevoie de urmatoarele sume: ")
+print(f"{necesitati} {ron} pentru necesitati uzuale")
+print(f"{recreative} {ron} pentru scopuri recreative")
+print(f"{economii} {ron} pentru economii")
