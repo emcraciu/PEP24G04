74e5bbda80e3f89c62a5a77e0609b7a6d3bb8076
emcraciu <ozy24us@gmail.com>
2024-10-28 21:12:07 +0200

    part3

---
+++ modul3/session2/app8.py
@@ -0,0 +1,7 @@
+shop1 = {"apples": 10, "plums": 13, "peach": 14, "banana": 10}
+shop2 = {"apples": 11, "plums": 14, "peach": 12}
+shop3 = {"apples": 12, "plums": 11, "peach": 15}
+
+cart = {"apples": 3, "plums": 4, "peach": 5}
+
+# where (which shop) do we get the best buy for our cart
