04622582a28812dcaf05004112b9eac2fb3e87f2
emcraciu <ozy24us@gmail.com>
2024-10-28 20:34:12 +0200

    part3

--- modul3/session2/app6.py
+++ modul3/session2/app6.py
@@ -5,3 +5,17 @@ for letter in word[1:]:
     if letter == first_letter:
         repeat += 1
 print(f'{first_letter} apare de {repeat} ori')
+
+# folosind dictionare
+
+word = input("Introduceti un cuvant fara majuscule: ")
+repeat = {}
+for letter in word:
+    if letter in repeat:
+        existing_repeat_count = repeat.get(letter)
+        existing_repeat_count += 1
+        repeat[letter] = existing_repeat_count
+    else:
+        repeat[letter] = 1
+for letter, repeat in repeat.items():
+    print(f"letter {letter} is repeated {repeat} times")
