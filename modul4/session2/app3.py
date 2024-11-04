b8ce3c3fac2d897a881af6779163a4d12d5c493b
emcraciu <ozy24us@gmail.com>
2024-11-04 21:43:48 +0200

    part3

--- modul4/session2/app3.py
+++ modul4/session2/app3.py
@@ -8,3 +8,41 @@ sine, dar acel interes nu era nimic pentru mine in comparatie cu continuarea de 
 mi-a oferit cel mai mare soc si surpriza din orice eveniment din vitța mea aventuroasa. Chiar si
 acum, dupa acest interval lung, mă trezesc emotionat cand ma gandesc la asta si simt din nou acel
 potop brusc de bucurie, uimire si neincredere care mi-a cufundat cu totul mintea."""
+
+
+def count_letters(python_text: str):
+    counter = 0
+    for letter in python_text:
+        if letter.isalpha():
+            counter += 1
+
+    return counter
+
+
+text_letters = count_letters(text)
+print(f"Letters are {text_letters} and all characters are {len(text)}")
+
+
+def get_words(python_text: str):
+    result = []
+    list_of_words = python_text.split()
+    for word in list_of_words:
+        new_word = word.strip(',.1234567890')
+        if new_word:
+            result.append(new_word)
+    return result
+
+
+list_of_words = get_words(text)
+print(list_of_words)
+
+
+def words_with_s(word_list: list[str]):
+    for word in word_list.copy():  # try not to use copy and debug what happens
+        if not word.lower().startswith('s'):
+            word_list.remove(word)
+    return word_list
+
+
+print(words_with_s(list_of_words))
+print(list_of_words)
