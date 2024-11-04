698e60e6ab0aa177cd6ae03e2f38f279adfe2c98
emcraciu <ozy24us@gmail.com>
2024-11-04 20:16:25 +0200

    part3

---
+++ modul4/session2/app2.py
@@ -0,0 +1,25 @@
+def suma(lista: list):
+    pass
+
+
+def medie(lista: list):
+    pass
+
+
+def putere(lista: list):
+    pass
+
+
+meniu = {
+    "1": medie,
+    "2": suma,
+    "3": putere
+}
+
+
+# a:
+def get_user_numbers():
+    pass
+
+
+get_user_numbers()
