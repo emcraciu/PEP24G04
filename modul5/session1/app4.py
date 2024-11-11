6021053a424fb0c469f7f95b13512323508b2123
emcraciu <ozy24us@gmail.com>
2024-11-11 21:40:20 +0200

    part5

--- modul5/session1/app4.py
+++ modul5/session1/app4.py
@@ -1,3 +1,6 @@
+import random
+
+
 def comparare(x, y):
     # functie care va compara numerele extrase cu cele alese
     pass
@@ -5,13 +8,24 @@ def comparare(x, y):
 
 def numere():
     # functie pentru extragerea numerelor
-    pass
+    all_49 = list(range(1, 50))
+    results = list()
+    for _ in range(6):
+        selection = random.choice(all_49)
+        all_49.remove(selection)
+        results.append(selection)
+    return results
 
 
 def alegere():
     # functie pentru alegerea numerelor de catre user
-    pass
+    return numere()
 
 
 while True:
     print("Se va juca 6/49. Alegeti 6 numere intre 1 si 49.")
+    user_numbers = alegere()
+    winning_numbers = numere()
+    guesed_number = comparare(user_numbers, winning_numbers)
+    if guesed_number == 6:
+        break
