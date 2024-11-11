2076b1c59b4dfe26f57223b03b8b2a1df663c9b0
emcraciu <ozy24us@gmail.com>
2024-11-11 21:18:23 +0200

    part5

---
+++ modul5/session1/app4.py
@@ -0,0 +1,17 @@
+def comparare(x, y):
+    # functie care va compara numerele extrase cu cele alese
+    pass
+
+
+def numere():
+    # functie pentru extragerea numerelor
+    pass
+
+
+def alegere():
+    # functie pentru alegerea numerelor de catre user
+    pass
+
+
+while True:
+    print("Se va juca 6/49. Alegeti 6 numere intre 1 si 49.")
