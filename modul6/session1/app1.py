f22840bb048403c93ea8e8869465e9f8f521293b
emcraciu <ozy24us@gmail.com>
2024-11-18 20:19:33 +0200

    part5

--- modul6/session1/app1.py
+++ modul6/session1/app1.py
@@ -1,10 +1,24 @@
 class Product:
-    pass
+
+    def __init__(self):
+        self.name, self.price, self.stock = input("Give (name, price, stock) for product:").split(',')
+
+    def __repr__(self):
+        pass
+
+    def __str__(self):
+        pass
 
 class Category:
 
     def __init__(self, name: str):
         self.category_name = name
+        self.products = []
+
+    def add_product(self):
+        # product = input("give product:")
+        product = Product()
+        self.products.append(product)
 
 class Shop:
     pass
