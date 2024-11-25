abaf9f034853944a8a20b049c1ca5847ea81c2bd
emcraciu <ozy24us@gmail.com>
2024-11-25 19:41:22 +0200

    part5

--- modul6/session1/app1.py
+++ modul6/session1/app1.py
@@ -1,7 +1,10 @@
 class Product:
 
-    def __init__(self):
-        self.name, self.price, self.stock = input("Give (name, price, stock) for product:").split(',')
+    def __init__(self, name=None, price=None, stock=None):
+        if name and price and stock:
+            pass
+        else:
+            self.name, self.price, self.stock = input("Give (name, price, stock) for product:").split(',')
 
     def __repr__(self):
         return (f"{self.__class__.__name__}: ({self.name},{self.price},{self.stock})")
@@ -31,11 +34,28 @@ class Category:
 class Shop:
     main_menu_message = 'Bun venit la magazinul Pycharm'
     main_menu_dict = {1: "Category", 2: "Produse", 3: "Iesire"}
+    product_menu_dict = {1: "Adaugare", 2: "Vizualizare", 3: "Iesire la meniul principal"}
     categories = [Category('pantofi'), Category('haine')]
 
     def products_menu(self):
         # code here
-        pass
+        while True:
+            print(40 * '=' + '\n' + Product.__name__.upper().center(40, '=') + '\n' + 40 * '=')
+            for key, value in self.product_menu_dict.items():
+                print(f'\t{key}: {value}')
+            selection = input('Introduceti optiunea:')
+            if selection == '1':
+                category = input("Introduceti numele categoriei:")
+                product = input("Introduceti numele produsului")
+                price = input("Introduceti pretul produsului")
+                stock = input("Introduceti stocul produsului")
+                for cat in self.categories:
+                    if cat.category_name == category:
+                        cat.products.append(Product(product, price, stock))
+            elif selection == '2':
+                print(self.categories)
+            elif selection == '3':
+                break
 
     def run(self):
         while True:
