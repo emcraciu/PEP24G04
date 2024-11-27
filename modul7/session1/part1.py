2d902ef51b118c56346284dd452bd07d4c0cad3f
emcraciu <ozy24us@gmail.com>
2024-11-27 21:12:49 +0200

    part5

--- modul7/session1/part1.py
+++ modul7/session1/part1.py
@@ -5,5 +5,37 @@ first_object = iterator.__next__()
 print(first_object)
 second_object = iterator.__next__()
 print(second_object)
-third_object = iterator.__next__()
-print(third_object)
+# third_object = iterator.__next__()
+# print(third_object)
+
+
+class TriangleIterator:
+    def __init__(self, sides: list):
+        self.sides = sides
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if not self.sides:
+            raise StopIteration
+        return self.sides.pop()
+
+
+class Triangle:
+    def __init__(self, A=1, B=1, C=1, AB=60, BC=60, CA=60):
+        self.A = A
+        self.B = B
+        self.C = C
+        self.AB = AB
+        self.BC = BC
+        self.CA = CA
+
+    def __iter__(self):
+        return TriangleIterator([('A', self.A), ('B', self.B), ('C', self.C)])
+
+
+t = Triangle()
+
+for side_name, side_value in t:
+    print(f'Side {side_name} has value: {side_value}')
