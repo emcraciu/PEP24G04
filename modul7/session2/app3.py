5a497c6d2d99c5c2b6da7e517a22f7487ef7b0d6
emcraciu <ozy24us@gmail.com>
2024-12-02 20:31:59 +0200

    part5

---
+++ modul7/session2/app3.py
@@ -0,0 +1,4 @@
+user_input = input("Introduceti salariile separate cu virgula: ")
+crestere_salariala = input("Introduceti procentul de marire: ")
+lista_salarii = user_input.split(",")
+lista_salarii_int = list(map())
