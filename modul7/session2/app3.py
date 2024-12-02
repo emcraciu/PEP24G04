598e54145e6e26086cf970a8436cc44dea6a951d
emcraciu <ozy24us@gmail.com>
2024-12-02 20:43:18 +0200

    part5

--- modul7/session2/app3.py
+++ modul7/session2/app3.py
@@ -1,4 +1,7 @@
 user_input = input("Introduceti salariile separate cu virgula: ")
-crestere_salariala = input("Introduceti procentul de marire: ")
+crestere_salariala = int(input("Introduceti procentul de marire: "))
 lista_salarii = user_input.split(",")
-lista_salarii_int = list(map())
+lista_salarii_int = list(map(lambda s: int(s), lista_salarii))
+salarii_marite = list(map(lambda s: s * (100 + crestere_salariala) / 100, lista_salarii_int))
+for salar_marit in salarii_marite:
+    print(salar_marit)
