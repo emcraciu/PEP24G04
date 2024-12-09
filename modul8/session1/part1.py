dbf32ecc51fcbfe82069d7434b7b9b9f202b99b7
emcraciu <ozy24us@gmail.com>
2024-12-09 18:48:49 +0200

    part5

--- modul8/session1/part1.py
+++ modul8/session1/part1.py
@@ -4,6 +4,13 @@
 
 # file operations
 
+file = open("C:/Users/ozy24/PycharmProjects/PEP24G04/modul8/session1/file.txt", 'wt')
+file.write("Hello Python")
+file.flush()
+file.close()
+
+with open("file1.txt", 'wt') as file:
+    file.write("Hello Python")
 
 
 # datetime
