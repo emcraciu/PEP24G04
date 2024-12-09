d05689f2ea80e9e08a7ae1b336595e583f72542f
emcraciu <ozy24us@gmail.com>
2024-12-09 21:19:47 +0200

    part5

--- modul8/session1/app3.py
+++ modul8/session1/app3.py
@@ -1,1 +1,29 @@
-# read data.txt
+from datetime import datetime
+
+
+class Elev:
+    def __init__(self, nume, data_nasterii: datetime):
+        self.name = nume
+        self.data_nasterii = data_nasterii
+
+    def __repr__(self):
+        return f'{self.name}:{self.data_nasterii}'
+
+    def days_to_birthday(self):
+        date_now = datetime.now()
+        date_birthday = datetime(date_now.year, self.data_nasterii.month, self.data_nasterii.day)
+        return date_birthday - date_now
+
+
+with open('data.txt', 'r') as file:
+    lines = file.readlines()
+elevi = list(
+    map(
+        lambda t: Elev(t.split("/")[0], datetime.strptime(t.split("/")[1].strip(), '%Y-%m-%d')),
+        lines
+    )
+)
+print(elevi)
+
+for elev in elevi:
+    print(elev.days_to_birthday())
--- modul8/session1/part1.py
+++ modul8/session1/part1.py
@@ -1,15 +1,14 @@
-# generators
-def my_generator(text: str):
-    for index, data in enumerate(text):
-        if data == '_':
-            return
-        yield f"{index}: {data}"
-
-result = my_generator('my_text')
-print(type(result))
-for letter in result:
-    print(letter)
-
+# # generators
+# def my_generator(text: str):
+#     for index, data in enumerate(text):
+#         if data == '_':
+#             return
+#         yield f"{index}: {data}"
+#
+# result = my_generator('my_text')
+# print(type(result))
+# for letter in result:
+#     print(letter)
 # # file operations
 #
 # file = open("C:/Users/ozy24/PycharmProjects/PEP24G04/modul8/session1/file.txt", 'wt')
@@ -27,3 +26,15 @@ for letter in result:
 
 
 # datetime
+import datetime
+
+date1 = datetime.datetime(2020, 10, 10)
+date2 = datetime.datetime(2020, 10, 8)
+print(date1 > date2)
+date_now = datetime.datetime.now()
+print(date_now)
+print(date_now - date1)
+print(type(date_now - date2))
+# print(datetime.datetime.now().strftime('%Y-%m-%d'))
+result = datetime.datetime.strptime('1998-4-1', '%Y-%m-%d')
+print(result)
